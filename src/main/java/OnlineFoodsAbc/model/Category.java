55b48a3e7526a0dc6afc07e66738d2c37e7fbc65
Thilina Shamika <thilinashamika@gmail.com>
2024-09-04 00:54:33 +0530

    Creating Entities

---
+++ src/main/java/OnlineFoodsAbc/model/Category.java
@@ -0,0 +1,20 @@
+package OnlineFoodsAbc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
---
+++ src/main/java/OnlineFoodsAbc/model/USER_ROLE.java
@@ -0,0 +1,8 @@
+package OnlineFoodsAbc.model;
+
+public enum USER_ROLE {
+
+    ROLE_CUSTOMER,
+    ROLE_RESTAURANT_OWNER,
+    ROLE_ADMIN
+}
